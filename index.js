ec219dca73ab58ee55e7ba3372abea944362349a
maxine-ellah & Rahmona Henry <pair+maxine-ellah.rahmona-henry@devbootcamp.com>
2016-04-04 11:16:35 +1200

    Updates

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var example = require('./views/example.hbs')
 xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
 
-  console.log(data.body)
+  var goodData = JSON.parse(data.body)
+  console.log(goodData[0].id)
   document.body.innerHTML = example({ name: "Monkey" });
 })
 
