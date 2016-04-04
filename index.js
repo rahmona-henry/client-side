b6c89d941c6300151e01cb9e42cd98e474f6dd13
maxine-ellah & Rahmona Henry <pair+maxine-ellah.rahmona-henry@devbootcamp.com>
2016-04-04 13:53:48 +1200

    Updates

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps=1436029892,1436029902&units=miles', function(err, data) {
+xhr.get('./cats.json', function(err, data) {
   if (err) console.log(err) // do something
 
-  var goodData = JSON.parse(data.body)
-  console.log(goodData[0].id)
-  document.body.innerHTML = example({ latitude: goodData[0].latitude });
+  var goodData = JSON.parse(data.body[0])
+  console.log(goodData.id)
+  document.body.innerHTML = example({ latitude: goodData[0].name });
 })
 
